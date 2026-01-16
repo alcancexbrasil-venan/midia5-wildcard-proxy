7d345ba0d265daf83fe7f4ad3907f24be4a08545
alcancexbrasil-venan <alcancexbrasil@gmail.com>
2026-01-16 14:43:16 -0300

    Add root index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  const host = req.headers.host || '';
+  const subdomain = host.split('.')[0];
+
+  res.status(200).send(`
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+      <head>
+        <meta charset="UTF-8" />
+        <title>${subdomain} | Media5</title>
+      </head>
+      <body style="font-family: Arial; padding: 40px;">
+        <h1>Site ativo: ${host}</h1>
+        <p>Rota raiz funcionando corretamente.</p>
+      </body>
+    </html>
+  `);
+}
